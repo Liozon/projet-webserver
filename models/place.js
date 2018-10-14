8e4bc90dac5680e639c973ee6b6e22a47d66762d
Stefanie Schlumpf <stefanie.schlumpf@heig-vd.ch>
2018-10-14 17:54:08 +0200

    complete list of constraints of the models user, trip and place

--- models/place.js
+++ models/place.js
@@ -1,8 +1,49 @@
+// TODO: placeGeolocalisation & placePicture in Schema
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-// Define the schema for users
-const userSchema = new Schema({
-  name: String
+
+/**
+ * Define the schema for places(ID, desciption, geolocalisation, picture, creation Date, last modification Date)
+ */
+const placeSchema = new Schema({
+    placeid: {
+        type: Number,
+        required: true,
+        unique: true,
+        validate: {
+            // Manually validate uniqueness to send a "pretty" validation error
+            validator: validatePlaceidUniqueness,
+            message: 'Place {VALUE} already exists'
+        }
+    },  
+    placeName: {
+        type: String,
+        required: 'Name of the place is required'
+    },
+    placeDescription: {
+        type: String
+    },
+    placeCreationDate: {
+        type: Date,
+        default: Date.now
+    },
+    placeLastModDate: {
+        type: Date,
+        default: Date.now
+    }
 });
+
+/**
+ * Given a place, calls the callback function with true if no place exists with that id
+ * (or the only place that exists is the same as the place being validated).
+ */
+function validatePlaceidUniqueness(value, callback) {
+  const place = this;
+  this.constructor.findOne().where('placeid').equals(value).exec(function(err, existingPlace) {
+    callback(!err && (!existingPlace || existingPlace._id.equals(place._id)));
+  });
+}
+
 // Create the model from the schema and export it
-module.exports = mongoose.model('place', userSchema);
+module.exports = mongoose.model('Place', placeSchema);
--- models/user.js
+++ models/user.js
@@ -1,39 +1,62 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Define the schema for users
+/**
+ * Define the schema for users(ID, username, email, password, registration Date)
+ */
 const userSchema = new Schema({
     userid: {
-        type: int,
+        type: Number,
         required: true,
         unique: true,
         validate: {
-            validator:validateUseridUniquness,
+            // Manually validate uniqueness to send a "pretty" validation error
+            validator: validateUseridUniqueness,
             message: 'User {VALUE} already exists'
         }
     }, 
-    username: {
-        type: string,
-        required: true,
+    userName: {
+        type: String,
+        required: 'Username is required',
         minlength: 3,
-        maxlength: 50,
+        maxlength: 30,
     },
     email: {
-        type: string,
-        required: true,
+        type: String,
+        required: 'Email adress is required',
+        validate: [validateEmail, 'Please fill a valid email adress']
     },
     password: {
-        type: string,
+        type: String,
         required: true,
         minlength: 8,
         maxlength: 20,
-        
     },
     registrationDate: {
-        type: date,
-        required: true,
-    },
+        type: Date,
+        default: Date.now
+    }
 });
 
+/**
+ * Verify if email is valid using regular expression
+ * Source: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+ */
+function validateEmail(email) {
+    var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(String(email).toLowerCase());
+}
+
+/**
+ * Given a name, calls the callback function with true if no person exists with that name
+ * (or the only person that exists is the same as the person being validated).
+ */
+function validateUseridUniqueness(value, callback) {
+  const user = this;
+  this.constructor.findOne().where('userid').equals(value).exec(function(err, existingUser) {
+    callback(!err && (!existingUser || existingUser._id.equals(user._id)));
+  });
+}
+
 // Create the model from the schema and export it
-module.exports = mongoose.model('user', userSchema);
+module.exports = mongoose.model('User', userSchema);
